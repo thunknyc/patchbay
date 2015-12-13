703641c0eb1cbb43e31f6e71390c08325e7cb044
Edwin Watkeys <edw@poseur.com>
2015-12-13 08:59:55 -0500

    Rewrite to improve readabiltiy and modularity
    Move publisher code to the end of the file; it was previously lost in
    the middle of the subscriber code.
    
    Rewrite `make-subscriber` to break the large single function into
    smaller parts. This requires using a box value for `sentinel`.
    
    Make various variables' names less unclear.

--- pubsub.scm
+++ pubsub.scm
@@ -1,44 +1,55 @@
 (use-modules
- ((rnrs bytevectors))
+ (rnrs bytevectors)
+ (srfi srfi-111)
  ((core scalability)
   #:renamer (symbol-prefix-proc 'nn:)))
 
 (define address "ipc:///tmp/pubsub.ipc")
 
 (define (make-subscriber address topics proc timeout-ms)
+
   "Connect to `address`, subscribe to `topics` and evaluate `proc`,
 presumably for side effects, for each received message as a
 bytevector. The subscriber will disconnect if `proc` returns the empty
 list. Returns a thunk which will, when evaluated, cause the subscriber
 to dissconnect within `timeout-ms`."
-  (let* ((sentinel #f)
-         (thread
-          (call-with-new-thread
-           (lambda ()
-             (let ((s (nn:pubsub-sub-socket)))
-               (nn:connect s address)
-               (apply nn:subscribe s topics)
-               (nn:set-recv-timeout s timeout-ms)
-               (let loop ((result (nn:call-with-recv-msg s proc)))
-                 (if (or (null? result) sentinel)
-                     (nn:shutdown s)
-                     (loop (nn:call-with-recv-msg s proc)))))))))
+
+  (define (make-socket address topics timeout)
+    (let ((s (nn:pubsub-sub-socket)))
+      (nn:connect s address)
+      (apply nn:subscribe s topics)
+      (nn:set-recv-timeout s timeout)
+      s))
+
+  (define (make-control-proc sentinel thread)
     (lambda (command)
       (case command
-        ((stop) (set! sentinel #t))
+        ((stop) (set-box! sentinel #t))
         ((join) (join-thread thread))
-        (else (format #t "SUBSCRIBER: UNKNOWN COMMAND ~s\n" command))))))
+        (else (format #t "SUBSCRIBER: UNKNOWN COMMAND ~s\n" command)))))
 
-(define (make-publisher addr)
-  (let ((s (nn:pubsub-pub-socket)))
-    (nn:bind s addr)
-    (lambda (msg)
-      (nn:send-string s msg))))
+  (define (process-message s)
+    (nn:call-with-recv-msg s proc))
 
-;; (define p (make-publisher address))
-;; (p "foo|bar")
+  (let* ((sentinel (box #f))
+         (thread (call-with-new-thread
+                  (lambda ()
+                    (let ((s (make-socket address topics timeout-ms)))
+                      (let loop ((result (process-message s)))
+                        (if (or (null? result) (unbox sentinel))
+                            (nn:shutdown s)
+                            (loop (process-message s)))))))))
+    (make-control-proc sentinel thread)))
 
-(define counter 0)
+(define recieved-message-counter 0)
+
+(define (monitor-received-message-counter)
+  (call-with-new-thread
+    (lambda ()
+      (let loop ()
+        (format #t "COUNTER: ~S\n" recieved-message-counter)
+        (usleep 1000000)
+        (loop)))))
 
 (define (run-subscriber)
  (let ((subscriber-ctl
@@ -46,14 +57,15 @@ to dissconnect within `timeout-ms`."
          address '("foo|")
          (lambda (s)
            (format #t "MSG: ~s ~s\n" (utf8->string s) s)
-           (set! counter (+ counter 1)))
+           (set! recieved-message-counter (+ recieved-message-counter 1)))
          1000)))
-   (call-with-new-thread
-    (lambda ()
-      (let loop ()
-        (format #t "COUNTER: ~S\n" counter)
-        (usleep 1000000)
-        (loop))))
    (subscriber-ctl 'join)))
 
+;; (begin (monitor-received-message-counter)
+;;        (run-subscriber))
 
+(define (make-publisher addr)
+  (let ((s (nn:pubsub-pub-socket)))
+    (nn:bind s addr)
+    (lambda (msg)
+      (nn:send-string s msg))))
