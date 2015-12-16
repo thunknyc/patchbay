f10002155896e1498e4605d5286a58b027eccf32
Edwin Watkeys <edw@poseur.com>
2015-12-16 09:51:45 -0500

    Improve readibility via better indentation

--- sub.scm
+++ sub.scm
@@ -13,11 +13,15 @@
 (call-with-new-thread
  (lambda ()
    (call-with-socket-endpoint
-    'sub '((receive-timeout 5000)) 'bind address
+    'sub '((receive-timeout 5000))
+    'bind address
     (lambda (s)
       (format #t "subscribe: ~s\n" (subscribe s "test|"))
-      (let loop ((result (call-with-received-string s process-message)))
-        (cond (result (set! message-count (+ message-count 1))
-                      (format #t "result: ~s\n" result))
-              (else (format #t "message-count: ~a\n" message-count)))
+      (let loop ((result
+                  (call-with-received-string s process-message)))
+        (cond (result
+               (set! message-count (+ message-count 1))
+               (format #t "result: ~s\n" result))
+              (else
+               (format #t "message-count: ~a\n" message-count)))
         (loop (call-with-received-string s process-message)))))))
